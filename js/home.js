9c0a814977868b2810052be88f65fa5297a1be1f
Uyen Nguyen <uyennguyen16900@gmail.com>
2019-07-31 13:31:42 -0700

    Edit javascript page

--- js/home.js
+++ js/home.js
@@ -1,7 +1,13 @@
-function validateForm() {
+const validateForm = () => {
   var x = document.forms["myForm"]["name"].value;
   if (x == "") {
     alert("Name must be filled out");
     return false;
   }
 }
+
+// let specialSection = document.getElementById('click')
+
+// const hightlight = (text) => {
+//   let
+// }
