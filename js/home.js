896ed527109b96eeaf35f952b2f03a811271e61e
Uyen Nguyen <uyennguyen16900@gmail.com>
2019-07-31 17:00:31 -0700

    Add buttons to the image slider

--- js/home.js
+++ js/home.js
@@ -6,8 +6,24 @@ const validateForm = () => {
   }
 }
 
-// let specialSection = document.getElementById('click')
+let varIndex = 1;
+showDivs(slideIndex);
 
-// const hightlight = (text) => {
-//   let
-// }
+const plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+const showDivs(n) {
+  let i;
+  let x = document.getElementByClassName("mySlide");
+  if (n > x.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = x.length;
+  }
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex-1].style.display = "block";
+}
