c41d36a8f0279d611496b8e01f3a29c275c0b877
Dangely <djeanelle0110@gmail.com>
2019-08-02 10:59:15 -0700

    Fixed background

---
+++ js/page.js
@@ -0,0 +1,8 @@
+setInterval(() => {
+  let body = document.querySelector('body');
+  let html = document.querySelector('html');
+  let height = Math.max( body.scrollHeight, body.offsetHeight,
+                         html.clientHeight, html.scrollHeight, html.offsetHeight );
+  body.style.height=height+"px";
+  html.style.height = height + 'px';
+}, 50);
